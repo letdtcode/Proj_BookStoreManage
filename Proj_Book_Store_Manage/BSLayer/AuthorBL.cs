54bff795397f643e588a32c09bd56c4c10079e27
do dung <dominhdung21082002@gmail.com>
2022-11-02 01:48:14 +0700

    delete BL Author

--- Proj_Book_Store_Manage/BSLayer/AuthorBL.cs
+++ Proj_Book_Store_Manage/BSLayer/AuthorBL.cs
@@ -11,33 +11,5 @@ namespace Proj_Book_Store_Manage.BSLayer
 {
     public class AuthorBL
     {
-        //DBMain db = null;
-        //SqlConnection conn = null;
-        //String nameView = "view_Author";
-
-        //public AuthorBL()
-        //{
-        //    db = new DBMain();
-        //    conn = new SqlConnection();
-        //}
-        //public DataTable getDataAuthor()
-        //{
-        //    return db.LoadData(nameView, CommandType.Text);
-        //}
-        //public bool addNewAuthor(string nameAuthor, string phoneNumber)
-        //{
-        //    String sqlString = "exec proc_addNewAuthor @nameAuthor = '" + nameAuthor + "', @phoneNumber = '" + phoneNumber + "'";
-        //    return db.ExecuteProcedure(sqlString, CommandType.Text);
-        //}
-        //public bool modifyAuthor(int idAuthor, string nameAuthor, string phoneNumber)
-        //{
-        //    String sqlString = "exec proc_updateAuthor @idAuthor = " + idAuthor + " @nameAuthor = '" + nameAuthor + "', @phoneNumber = '" + phoneNumber + "'";
-        //    return db.ExecuteProcedure(sqlString, CommandType.Text);
-        //}
-        //public bool deleteAuthor(int idAuthor)
-        //{
-        //    String sqlString = "exec proc_DeleteAuthor @idAuthor = " + idAuthor;
-        //    return db.ExecuteProcedure(sqlString, CommandType.Text);
-        //}
     }
 }
