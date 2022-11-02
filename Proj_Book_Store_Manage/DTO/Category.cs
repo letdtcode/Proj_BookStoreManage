686e512c7135803f1cfddf05849fa8d9377ad9e5
Tân Nguyễn Thái Ngọc <you@example.com>
2022-11-02 22:57:00 +0700

    Update

---
+++ Proj_Book_Store_Manage/DTO/Category.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj_Book_Store_Manage.DTO
+{
+    public class Category
+    {
+
+        private int idCategory;
+        private string nameCategory;
+        private string description;
+
+        public int IdCategory { get => idCategory; set => idCategory = value; }
+        public string NameCategory { get => nameCategory; set => nameCategory = value; }
+        public string Description { get => description; set => description = value; }
+        public Category()
+        {
+
+        }
+
+        public Category(DataRow data)
+        {
+            this.IdCategory = (int)data["idCategory"];
+            this.NameCategory = (string)data["nameCategory"];
+            this.Description = (string)data["description"];
+
+        }
+    }
+}
