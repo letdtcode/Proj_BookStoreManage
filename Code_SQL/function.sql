5721f6749ab0de3205aa972a1e19e9316f4f497e
MinhDung08 <dominhdung21082002@gmail.com>
2022-11-11 16:34:48 +0700

    update func git info Employee

--- Code_SQL/function.sql
+++ Code_SQL/function.sql
@@ -241,22 +241,25 @@ go
 create or alter function func_getIdEmployee (@user varchar(20), @password varchar(30))
 returns @table table (
 	idEmp varchar(8),
-	nameEmp nvarchar (40)
+	nameEmp nvarchar (40),
+	idRole bit
 	)
 as
 begin
-	declare @idEmp varchar(8), @firstName nvarchar(10), @middleName nvarchar(10), @lastName nvarchar (10), @name nvarchar (40)
+	declare @idEmp varchar(8), @firstName nvarchar(10), @middleName nvarchar(10), @lastName nvarchar (10), @name nvarchar (40), @idRole bit
 	select @idEmp = ACCOUNT.idEmployee,
 	@middleName = middleName,
 	@firstName = firstName,
-	@lastName = lastName from ACCOUNT, EMPLOYEE 
+	@lastName = lastName,
+	@idRole = typeOfAcc
+	from ACCOUNT, EMPLOYEE 
 	where  nameAccount=@user and
 			password = @password and
 			ACCOUNT.idEmployee = EMPLOYEE.idEmployee
 
 			
 	set @name = @firstName + ' ' + @middleName + ' ' + @lastName
-	insert into @table values (@idEmp, @name)
+	insert into @table values (@idEmp, @name, @idRole)
 	return
 end
 go
